b2e741d53cfc2ef416bd12dd18772565050ccd94
dustin-chan-89 <busterdust@gmail.com>
2016-07-26 00:12:00 -0700

    making functions to get profile data

--- OverwatchApps/static/comparison.js
+++ OverwatchApps/static/comparison.js
@@ -1,23 +1,41 @@
-var playerOne;
-var playerTwo;
+var playerOneProfileData;
+var playerOneCharacterData = [];
+
+var playerTwoProfileData;
+var playerTwoCharacterData = [];
+
+var offenseHeroes = "Genji,McCree,Pharah,Reaper,Soldier76,Tracer";
+var defenseHeroes = "Bastion,Hanzo,Junkrat,Mei,Torbjoern,Widowmaker";
+var tankHeroes = "D.Va,Reinhart,Roadhog,Winston,Zarya";
+var supportHeroes = "Ana,Lucio,Mercy,Symmetra,Zenyatta";
+
+var allHeroes = [offenseHeroes, defenseHeroes, tankHeroes, supportHeroes];
+
+//level, games won/lost, playtime, avatar_img, rank, rank_img
+function getProfileData(battleTag, platform, region) {
+  $.ajax({
+    url: "https://api.lootbox.eu/"+platform+"/"+region+"/"+battleTag+"/profile",
+    data: [],
+    success: function(json){
+      return json.data;
+    },
+    dataType: "json"
+  });
+}
+
+function getCharacterData(battleTag, platform, region, mode) {
+  var allHeroesData = [];
+  allHeroes.forEach(function(heroGroup){
+    $.ajax({
+      url: "https://api.lootbox.eu/"+platform+"/"+region+"/"+battleTag+"/"+mode+"/hero-multiple/"+heroGroup+"/",
+      data: [],
+      success: function(json){
+        return json.data;
+      }
+    });
+  });
+}
 
 $(document).on('click', '#execute-player-search', function(){
 
-  $.ajax({
-    url: "https://api.lootbox.eu/pc/us/WorstHanzoNA-1566/profile",
-    data: [],
-    success: function(json){
-      playerOne = json.data;
-    },
-    dataType: "json"
-  });
-
-  $.ajax({
-    url: "https://api.lootbox.eu/pc/us/Mujang-11771/profile",
-    data: [],
-    success: function(json){
-      playerTwo = json.data;
-    },
-    dataType: "json"
-  });
 });
