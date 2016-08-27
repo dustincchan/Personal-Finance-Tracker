564668e61fe94f58f9fbee11b22c90167031f8de
dustin-chan-89 <busterdust@gmail.com>
2016-08-26 21:26:54 -0700

    added some nice and easy django authentication

--- PersonalFinance/urls.py
+++ PersonalFinance/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin, auth
+from django.views.generic import RedirectView
 import views
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', auth.views.login),
     url(r'^logout/$', auth.views.logout),
+    url(r'^accounts/profile/$', RedirectView.as_view(url='/')),
 ]
